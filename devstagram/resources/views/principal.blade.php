6a97603024021fcf79f3d39fe00d7a77fa48430c
SG-WORK <gpsara1114@gmail.com>
2024-12-04 17:28:48 -0600

    Pruebas de layout

---
+++ devstagram/resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Home
+@endsection
+
+@section('contenido')
+    Aqui va el contenido del header
+@endsection
---
+++ devstagram/resources/views/tienda.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Tienda viertual
+@endsection
+
+@section('contenido')
+    Nuestra tienda
+@endsection
