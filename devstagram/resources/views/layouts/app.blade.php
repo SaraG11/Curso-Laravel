7e82be3ed2b850bf29affbf2cc9ff152b4331e2a
SG-WORK <gpsara1114@gmail.com>
2024-12-04 18:47:37 -0600

    Inicio de proyecto devstagram, instalacion de Tailwind

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -3,11 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>DevStagram - @yield('titulo')</title>
+        @vite('resources/css/app.css')
 
     </head>
-    <body>
-        
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevStagram</h1>
+                <nav>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                </nav>
+            </div>
+        </header>
     </body>
 </html>
