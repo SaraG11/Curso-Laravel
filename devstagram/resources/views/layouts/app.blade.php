b890afd5c052473cea7457f6eaa7860fd518254b
SG-WORK <gpsara1114@gmail.com>
2024-12-10 17:03:27 -0600

    Controladores y vistas

--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -13,9 +13,19 @@
                 <h1 class="text-3xl font-black">DevStagram</h1>
                 <nav>
                     <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Crear Cuenta</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
                 </nav>
             </div>
         </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main>
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram todos los derechos reservados @php echo date('Y')    @endphp
+        </footer>
     </body>
 </html>
