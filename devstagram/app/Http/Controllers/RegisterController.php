ebd752c75d94d171a4aa25e670f23075cd147c32
SG-WORK <gpsara1114@gmail.com>
2024-12-19 12:22:45 -0600

    Inicio de proyecto, hasta logout

--- devstagram/app/Http/Controllers/RegisterController.php
+++ devstagram/app/Http/Controllers/RegisterController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,8 +15,36 @@ class RegisterController extends Controller
     {
         return view('auth.register');
     }
-    public function store()
+    public function store(Request $request)
     {
-        dd('Post...');
+        // dd($request->get('name'));
+        // Modificar request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        // Validacion
+        $request->validate([
+            'name' => 'required|max:20',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        // dd('Creando Usuario...');
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Autenticar un usuario
+        // Auth::attempt(['email' => $request->email, 'password' => $request->password]);
+
+        // Otra forma de autenticar 
+        Auth::attempt($request->only('email', 'password'));
+
+        // Reedireccionar al usuario
+        return redirect()->route('post.index');
+
     }
 }
--- devstagram/resources/views/layouts/app.blade.php
+++ devstagram/resources/views/layouts/app.blade.php
@@ -11,10 +11,21 @@
         <header class="p-5 border-b bg-white shadow">
             <div class="container mx-auto flex justify-between items-center">
                 <h1 class="text-3xl font-black">DevStagram</h1>
-                <nav>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
-                </nav>
+                @auth
+                    <nav>
+                        <a class="font-bold text-gray-600 text-sm" href="">
+                            Hola: <span class="font-normal">{{ auth()->user()->username }}</span>
+                        </a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('logout') }}">Cerrar Sesion</a>
+                    </nav>
+                @endauth
+                @guest
+                    <nav>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="">Login</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta</a>
+                    </nav>
+                @endguest
+                
             </div>
         </header>
 
