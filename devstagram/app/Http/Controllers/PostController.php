8124187cfe62fcbfa6d250ccfb84414aa6e7c8e7
SG-WORK <gpsara1114@gmail.com>
2025-01-21 16:25:26 -0600

    Creacion de posts, eliminacion de posts, policy, base de datos relacion usuario posts

--- devstagram/app/Http/Controllers/PostController.php
+++ devstagram/app/Http/Controllers/PostController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Post;
+
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Routing\Controller as BaseController;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+
+    }
     
     public function index(User $user){
 
@@ -26,6 +33,8 @@ class PostController extends Controller
     {
         return view('posts.create');
     }
+
+
     public function store(Request $request)
     {
         $request->validate([
@@ -52,4 +61,25 @@ class PostController extends Controller
 
         return redirect()->route('posts.index', Auth::user()->username);
     }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post ,
+            'user' => $user
+        ]);
+    }
+
+    public function destroy(Post $post){
+        $this->authorize('delete', $post);
+        $post->delete();
+        // Eliminar imagen
+        $imagen_path = public_path('uploads/' . $post->imagen);
+        if(File::exists($imagen_path)){
+            unlink($imagen_path);
+        }
+
+        return redirect()->route('posts.index', Auth::user()->username);
+    }
+
 }
